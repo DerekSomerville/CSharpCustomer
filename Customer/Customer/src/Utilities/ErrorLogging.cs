bc031b4b1aecb93cebe4a3cd5a6adcdee40295d3
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-02-23 10:00:01 +0000

    Test Change

--- Customer/Customer/src/Utilities/ErrorLogging.cs
+++ Customer/Customer/src/Utilities/ErrorLogging.cs
@@ -27,7 +27,7 @@ namespace Customer.src.Utilities
 
         public void writeToLog(string method, string message)
         {
-            Console.WriteLine("Method: " + method + " message: " + message);
+            Console.WriteLine("Method: " + method + " Message: " + message);
             this.writeErrorLog.write(method + "," + message);
         }
     }
